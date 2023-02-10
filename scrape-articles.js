72cd8270d55368664aff9e1af7a4fb0f9049d252
Vincent Woo <me@vincentwoo.com>
2023-02-10 03:00:47 -0800

    Just scrape all the articles

---
+++ scrape-articles.js
@@ -0,0 +1,49 @@
+const cheerio = require("cheerio");
+const url = "https://en.wikipedia.org/wiki/Wikipedia:Vital_articles/Level/4/";
+const categories = [
+  "People",
+  "History",
+  "Geography",
+  "Arts",
+  "Philosophy and religion",
+  "Everyday life",
+  "Society and social sciences",
+  "Biology and health sciences",
+  "Physical sciences",
+  "Technology",
+  "Mathematics",
+];
+
+async function scrapeArticles() {
+  const pages = await Promise.all(
+    categories.map(async (category) => {
+      const resp = await fetch(url + category);
+      const $ = cheerio.load(await resp.text());
+      const results = [];
+      $("#bodyContent li a").each(function(idx, elem) {
+        if (
+          $(this).attr("href") &&
+          $(this)
+            .attr("href")
+            .startsWith("/wiki/") &&
+          !$(this)
+            .attr("href")
+            .includes(":")
+        )
+          results.push(
+            $(this)
+              .attr("href")
+              .split("/wiki/")[1]
+          );
+      });
+      return [category, results];
+    })
+  );
+  const result = {};
+  for (const [category, articles] of pages) {
+    result[category] = articles;
+  }
+  console.log(`window.articles = ${JSON.stringify(result)}`);
+}
+
+scrapeArticles();
